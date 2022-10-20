6fe3bed28853ce5761de4fb46ccc1dd36bd7b09e
Abdulmalik <sanusiu111@gmail.com>
2022-10-20 16:36:35 -0700

    more

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,22 +13,17 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3) == 0)
+		if (i % 3 == 0)
 			printf("Fizz");
-		else if ((i % 5) == 0)
+		else if (i % 5 == 0)
 			printf("Buzz");
-		else if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
+		else if (i % 15 == 0)
+			printf("Buzz");
 		else
-			printf("%d", i);
-
-		if (num != 100)
+			printf("%i", i);
+		if (i < 100)
 			printf(" ");
-		else
-			printf("\n");
 	}
 	printf("\n");
-
 	return (0);
 }
-
