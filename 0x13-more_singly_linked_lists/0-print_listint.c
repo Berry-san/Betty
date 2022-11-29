9964c5a287367764510ae9e30ddcf38fcd9fa50e
Abdulmalik <sanusiu111@gmail.com>
2022-11-28 17:29:24 -0800

    0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,12 +8,11 @@
  *
  * Return: the number of nodes
  */
-
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
@@ -21,4 +20,3 @@ size_t print_listint(const listint_t *h)
 	}
 	return (count);
 }
-
