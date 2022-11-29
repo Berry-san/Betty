a4ac75fc2c28ee1799d8f15b21328144d584ffae
Abdulmalik <sanusiu111@gmail.com>
2022-11-28 16:37:00 -0800

    corrections

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -16,7 +16,7 @@ listint_t **_ra(listint_t **list, size_t size, listint_t *new)
 	listint_t **newlist;
 	size_t i;
 
-	newlist = malloc(sizesizeof(listint_t *));
+	newlist = malloc(size * sizeof(listint_t *));
 	if (newlist == NULL)
 	{
 		free(list);
