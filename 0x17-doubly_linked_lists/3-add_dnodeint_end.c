8d1aa1eeb784c69fdcf12cefe79050e1c0e16226
Abdulmalik <sanusiu111@gmail.com>
2023-01-12 16:37:21 -0800

    doubly

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -7,7 +7,7 @@
  * @n: value to add to new node
  *
  * Return: pointer to the new node, or NULL on failure
- * */
+ */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new, *tmp;
